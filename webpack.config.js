5a784dbd5cfea5528615cb024dc0f355b0a3ed31
Oleg Gudzii <oleg198679@gmail.com>
2019-08-14 02:50:22 +0300

    Add and configure postcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: {
     server: './server/index.js',
+    main: './src/index.js',
   },
   output: {
     path: path.join(__dirname, 'dist'),
@@ -21,6 +23,20 @@ module.exports = {
         use: {
           loader: 'babel-loader',
         },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixer({})],
+            },
+          },
+        ],
       },
     ],
   },
