034c7e5617bd80bbb6729785792fd5dbfee31e1d
Oleg Gudzii <oleg198679@gmail.com>
2019-09-05 15:18:22 +0300

    Tests: add user routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,6 +18,18 @@ describe('requests', () => {
     const res = await request.agent(server)
       .get('/');
     expect(res).toHaveHTTPStatus(200);
+
+    const res2 = await request.agent(server)
+      .get('/users');
+    expect(res2).toHaveHTTPStatus(200);
+
+    const res3 = await request.agent(server)
+      .get('/users/new');
+    expect(res3).toHaveHTTPStatus(200);
+
+    const res4 = await request.agent(server)
+      .post('/users');
+    expect(res4).toHaveHTTPStatus(308);
   });
 
   it('GET 404', async () => {
