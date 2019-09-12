79db3d8d8ffc1ecf48cb1894769325e9a5b97490
Oleg Gudzii <oleg198679@gmail.com>
2019-09-13 01:39:01 +0300

    Remve unnecessary logging

--- routes/users.js
+++ routes/users.js
@@ -9,18 +9,14 @@ export default (router) => {
     })
     .get('newUser', '/users/new', async (ctx) => {
       const user = User.build();
-      console.log(user);
       await ctx.render('users/new', { f: buildFormObj(user) });
     })
     .post('users', '/users', async (ctx) => {
       const { request: { body: form } } = ctx;
       const user = User.build(form);
       try {
-        console.log('saving user');
         await user.save();
-        console.log('prepare message');
         await ctx.flash.set('User has been created');
-        console.log('redirecting');
         await ctx.redirect(router.url('root'));
       } catch (e) {
         await ctx.render('users/new', { f: buildFormObj(user, e) });
