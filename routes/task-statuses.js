50c85ab43df0b21c5442edd996961f1c17f522a8
Oleg Gudzii <oleg198679@gmail.com>
2019-09-19 16:48:13 +0300

    Add status creation

--- routes/task-statuses.js
+++ routes/task-statuses.js
@@ -1,10 +1,29 @@
-// import buildFormObj from '../lib/formObjectBuilder';
+import buildFormObj from '../lib/formObjectBuilder';
 import { TaskStatus } from '../models';
 
 export default (router) => {
   router
-    .get('taskStatuses', '/task-statuses', async (ctx) => {
+    .get('taskStatuses', '/statuses', async (ctx) => {
       const statuses = await TaskStatus.findAll();
       await ctx.render('statuses', { statuses });
+    })
+    .get('newStatus', '/statuses/new', async (ctx) => {
+      const status = TaskStatus.build();
+      await ctx.render('statuses/new', { f: buildFormObj(status) });
+    })
+    .post('taskStatuses', '/statuses', async (ctx) => {
+      const {
+        request: {
+          body: { form },
+        },
+      } = ctx;
+      const status = TaskStatus.build(form);
+      try {
+        await status.save();
+        ctx.flash.set('New status created');
+        await ctx.redirect(router.url('taskStatuses'));
+      } catch (e) {
+        await ctx.render('statuses/new', { f: buildFormObj(status, e) });
+      }
     });
 };
